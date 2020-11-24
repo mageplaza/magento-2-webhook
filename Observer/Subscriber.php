c0bfcc2c0790bfbc1ff776628dbd285ae761f879
Peter Jaap Blaakmeer <peterjaap@blaakmeer.com>
2020-11-24 09:41:20 +0100

    Also fire webhook on unsubscribe

--- Observer/Subscriber.php
+++ Observer/Subscriber.php
@@ -23,6 +23,7 @@ namespace Mageplaza\Webhook\Observer;
 
 use Exception;
 use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Newsletter\Model\Subscriber as SubscriberMagento;
 use Magento\Store\Model\Store;
@@ -42,18 +43,13 @@ class Subscriber extends AfterSave
 
     /**
      * @param Observer $observer
-     *
      * @return $this|void
      * @throws NoSuchEntityException
+     * @throws LocalizedException
      */
     public function execute(Observer $observer)
     {
         $item = $observer->getEvent()->getSubscriber();
-        $subscriberStatus = $item->getSubscriberStatus();
-
-        if ($subscriberStatus === SubscriberMagento::STATUS_UNSUBSCRIBED) {
-            return $this;
-        }
 
         if ($this->helper->getCronSchedule() !== Schedule::DISABLE) {
             $hookCollection = $this->hookFactory->create()->getCollection()
